853bf6233081ea8e5c308c873681eb1329efc65b
aphavichitr <andrew.phavichitr@gmail.com>
2016-05-11 16:13:43 -0700

    Add functionality for serving index.html & implement readListOfUrls

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,22 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelp = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var url = req.url;
+  var method = req.method;
+  console.log(method, 'request made for ', url);
+
+  if (method === 'GET') {
+    if (url === '/') {
+      httpHelp.serveAssets(res, url);
+    } else {
+      if (archive.isUrlInList(url)) {
+        console.log('Found', url);
+      }
+    }
+  }
+
+  // res.end(archive.paths.list);
 };
